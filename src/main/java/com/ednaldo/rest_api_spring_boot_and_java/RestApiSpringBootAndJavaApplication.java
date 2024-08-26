1bdca9b6a987eccab091e51cab6a7ae7c4ba944d
Miguelito2926 <ednaldo.junior@prill.com.br>
2024-08-26 18:41:42 -0300

    vreate project

---
+++ src/main/java/com/ednaldo/rest_api_spring_boot_and_java/RestApiSpringBootAndJavaApplication.java
@@ -0,0 +1,13 @@
+package com.ednaldo.rest_api_spring_boot_and_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiSpringBootAndJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiSpringBootAndJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ednaldo/rest_api_spring_boot_and_java/RestApiSpringBootAndJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ednaldo.rest_api_spring_boot_and_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiSpringBootAndJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

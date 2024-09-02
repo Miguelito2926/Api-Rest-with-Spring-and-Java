dacf9e86c041300a5e0400ee9583df1d2fc57982
Miguelito2926 <ednaldo.junior@prill.com.br>
2024-09-02 19:53:49 -0300

    feat:[WIP] Hablitation cors global in project

--- src/main/java/com/ednaldo/rest_api_spring_boot_and_java/config/AppConfig.java
+++ src/main/java/com/ednaldo/rest_api_spring_boot_and_java/config/AppConfig.java
@@ -1,10 +1,12 @@
 package com.ednaldo.rest_api_spring_boot_and_java.config;
 
 import com.ednaldo.rest_api_spring_boot_and_java.serialization.converte.YamlJackosn2HttpMessageConvert;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -14,6 +16,8 @@ public class AppConfig implements WebMvcConfigurer {
 
     private static final MediaType MEDIA_TYPE_APPLICATION_YML = MediaType.valueOf("application/x-yaml");
 
+    @Value("${cors.originPatterns:default}")
+    private String corsOriginPatterns = "";
     @Override
     public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
 
@@ -31,7 +35,18 @@ public class AppConfig implements WebMvcConfigurer {
     public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
         converters.add(new YamlJackosn2HttpMessageConvert());
     }
-//        Via Query Param. http://localhost:8080/v1/persons?mediaType=xml
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        var allowedOrigins = corsOriginPatterns.split(",");
+        registry.addMapping("/**")
+                //.allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedMethods("*")
+                .allowedOrigins(allowedOrigins)
+                .allowCredentials(true);
+    }
+
+    //        Via Query Param. http://localhost:8080/v1/persons?mediaType=xml
 //        configurer.favorParameter(true)
 //                .parameterName("mediaType").ignoreAcceptHeader(true)
 //                .useRegisteredExtensionsOnly(false)

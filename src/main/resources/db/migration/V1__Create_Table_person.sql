297c746eec8ec9703a141aa996a8d43234af8ed4
Miguelito2926 <ednaldo.junior@prill.com.br>
2024-09-12 19:15:11 -0300

    feat:[WIP] Update in table Person

--- src/main/resources/db/migration/V1__Create_Table_person.sql
+++ src/main/resources/db/migration/V1__Create_Table_person.sql
@@ -1,14 +1,14 @@
-
 CREATE TABLE IF NOT EXISTS `pessoa` (
-  `id` bigint NOT NULL AUTO_INCREMENT,
-  `nome` varchar(50) NOT NULL,
-  `sobrenome` varchar(50) NOT NULL,
-  `email` varchar(100) UNIQUE,
-  `telefone` varchar(15),
-  `endereco` varchar(150),
-  `sexo` varchar(10),
-  `data_nascimento` datetime,
-  `data_criacao` timestamp DEFAULT CURRENT_TIMESTAMP,
-  `data_atualizacao` timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `id` BIGINT NOT NULL AUTO_INCREMENT,
+  `nome` VARCHAR(50) NOT NULL,
+  `sobrenome` VARCHAR(50) NOT NULL,
+  `email` VARCHAR(100) UNIQUE,
+  `telefone` VARCHAR(15),
+  `endereco` VARCHAR(150),
+  `sexo` VARCHAR(1) NOT NULL,  -- Mantido como VARCHAR(1) para valores 'M' e 'F'
+  `ativo` TINYINT(1) DEFAULT 1,    -- Mantido como TINYINT(1) para valores booleanos
+  `data_nascimento` DATE,          -- Mantido como DATE para compatibilidade com datas
+  `data_criacao` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  `data_atualizacao` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 );

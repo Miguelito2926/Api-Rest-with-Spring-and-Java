3f9a80433452de478d28043d1a20c58e752039cb
Miguelito2926 <ednaldo.junior@prill.com.br>
2024-09-27 20:52:52 -0300

    implementando serviço de adição de livros

--- client/src/pages/Login/index.js
+++ client/src/pages/Login/index.js
@@ -1,8 +1,38 @@
 import './styles.css'; // Certifique-se de que o CSS global está sendo importado aqui.
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom'; 
 import Navbar from '../Navbar/index';
+import api from '../../services/api';
 
 export default function Login() {
+
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const navigate = useNavigate();
+
+    async function login(event) {
+        event.preventDefault();
+
+        const data = {
+            username,
+            password,
+        };
+
+        try {
+            const response = await api.post('auth/signin', data);
+            
+            // Corrigindo o nome da chave do token
+            localStorage.setItem('username', username);
+            localStorage.setItem('accessToken', response.data.token);
+
+            // Navegar para a rota 'books'
+            navigate('/');
+            
+        } catch (error) {
+            alert('Login ou Senha inválida!');
+        }
+    };
+
     useEffect(() => {
         // Adicionar a classe de fundo ao body quando o componente é montado
         document.body.classList.add('login-background');
@@ -16,13 +46,23 @@ export default function Login() {
     return (
         <div>
             <Navbar />           
-            <div className="login-container" style={{ padding: '20px', marginTop: '64px' }}> {/* Adiciona margem para não ficar atrás da navbar */}
+            <div className="login-container" style={{ padding: '20px', marginTop: '64px' }}>
                 <section className="form">
                     <h1>Login</h1>
-                    <form>
+                    <form onSubmit={login}>
                         <div className="input-wrapper">
-                            <input type="text" placeholder="Email" />
-                            <input type="password" placeholder="Senha" />
+                            <input 
+                                type="text" 
+                                placeholder="Email" 
+                                value={username} 
+                                onChange={event => setUsername(event.target.value)}
+                            />
+                            <input 
+                                type="password" 
+                                placeholder="Senha" 
+                                value={password} 
+                                onChange={event => setPassword(event.target.value)} 
+                            />
                             <button className="button" type="submit">Entrar</button>
                         </div>
                     </form>
--- client/src/pages/NewBook/index.js
+++ client/src/pages/NewBook/index.js
@@ -1,10 +1,46 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './style.css';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
 import Navbar from '../Navbar';
+import api from '../../services/api';
 
 export default function NewBook() {
+
+  const [id, setId] = useState(null);
+  const [autor, setAuthor] = useState('');
+  const [data_lancamento, setLaucnhDate] = useState('');
+  const [preco, setPrice] = useState('');
+  const [titulo, setTitle] = useState(''); 
+
+  const accessToken = localStorage.getItem('accessToken');
+
+  const navigate = useNavigate();
+
+  async function createNewBook(event) {
+    event.preventDefault();
+  
+    const data = {
+      titulo,
+      autor,
+      data_lancamento,
+      preco,
+    };
+  
+    const headers = {
+      Authorization: `Bearer ${accessToken}`,
+    };
+  
+    try {     
+      await api.post('v1/books', data, { headers });
+      alert('Livro criado com sucesso!');
+      navigate('/books');
+    } catch (error) {
+      alert('Erro ao gravar livro. Tente novamente!');
+    }
+  }
+  
+
   return (
     <div className="new-book-container" style={{ marginTop: '64px' }}> {/* Adiciona margem para não ficar atrás da navbar */}
       <Navbar/>
@@ -19,11 +55,11 @@ export default function NewBook() {
             </Link>
           </section>
 
-          <form>
-            <input placeholder="Título" />
-            <input placeholder="Autor" />
-            <input type="date" />
-            <input placeholder="Preço" />
+          <form onSubmit={createNewBook}>
+            <input placeholder="Título" value={titulo} onChange={event => setTitle(event.target.value)} />
+            <input placeholder="Autor" value={autor} onChange={event => setAuthor(event.target.value)} />
+            <input type="date"value={data_lancamento} onChange={event => setLaucnhDate (event.target.value)} />
+            <input placeholder="Preço" value={preco} onChange={event =>  setPrice(event.target.value)} />
 
             <button className="button" type='submit'>Adicionar</button>
           </form>
